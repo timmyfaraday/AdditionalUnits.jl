f67bca04f0865b7328e23e404f6c8ee1a00dd891
Tom Van Acker <van_acker_tom@me.com>
2021-01-28 12:52:57 +0100

    Initial Commit
    addition of Currency, euro and Wh

--- src/AdditionalUnits.jl
+++ src/AdditionalUnits.jl
@@ -1,5 +1,25 @@
 module AdditionalUnits
 
-# Write your package code here.
+# using pkgs
+using Unitful
+
+# pkg constants
+const _UF = Unitful
+
+# dimensions
+@dimension          𝐂           "𝐂"         Currency
+
+# units
+@refunit            €           "€"         Euro        𝐂           true
+@unit               Wh          "Wh"        WattHour    3600.0u"J"  true     
+
+# init function to make this work at run-time
+const localunits = _UF.basefactors
+const localpromotion = _UF.promotion
+function __init__()
+    merge!(_UF.basefactors, localunits)
+    merge!(_UF.promotion, localpromotion)
+    _UF.register(AdditionalUnits)
+end
 
 end
