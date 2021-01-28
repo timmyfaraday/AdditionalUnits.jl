4e76624414c9f33065d0daf1b2570fda8e399346
Tom Van Acker <van_acker_tom@me.com>
2021-01-28 11:53:39 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/AdditionalUnits.jl
@@ -0,0 +1,5 @@
+module AdditionalUnits
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AdditionalUnits
+using Test
+
+@testset "AdditionalUnits.jl" begin
+    # Write your tests here.
+end
